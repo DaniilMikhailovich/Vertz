6491b1bc29795f047cebd6fd4ee0b033525928bc
orangefruit21 <borovskih.05021999@gmail.com>
2019-10-07 01:33:16 +0300

    Merge

---
+++ php/footer.php
@@ -0,0 +1,22 @@
+<footer>
+	<a href="#">
+		<img src="src/img/Logo-footer.png" alt="logotipe">
+	</a>
+	<ul>
+		<li><h2>О студии</h2></li>
+		<li>Наша команда</li>
+		<li>Почему мы?</li>
+		<li>Обратная связь</li>
+		<li>Контакты</li>
+	</ul>
+	<ul>
+		<li><h2>Каталог</h2></li>
+		<li>Корпусная мебель</li>
+		<li>Кухни</li>
+		<li>Изделия из камня</li>
+	</ul>
+	<article>
+		<span>All rights reserved</span>
+		<a href="">Made by SubAttr</a>
+	</article>
+</footer>
